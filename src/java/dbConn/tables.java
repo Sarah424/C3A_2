f6a6a870c81856e13811d14014151cbcf0712159
AtheerAli <74508276+athy1999@users.noreply.github.com>
2022-11-02 18:36:31 +0300

    Create tables.java

---
+++ src/java/dbConn/tables.java
@@ -0,0 +1,11 @@
+//CREATE TABLE `trip` (
+//  `id` INT NOT NULL AUTO_INCREMENT,
+//  `Admin_id` VARCHAR(45) NULL,
+//  `User_id` VARCHAR(45) NULL,
+//  `Price` INT NULL,
+//  `gate` VARCHAR(45) NULL,
+//  `Departure_station` VARCHAR(45) NULL,
+//  `Arrival_station` VARCHAR(45) NULL,
+//  `date` DATETIME NULL,
+//  `time` DATETIME NULL,
+//  PRIMARY KEY (`id`));
